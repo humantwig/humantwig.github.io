076f3259fb60a9466c78363a1ec6cd16840493c3
Jacob Lucas <57728735+humantwig@users.noreply.github.com>
2022-06-26 00:14:58 +1200

    Add files via upload

---
+++ SPILL/script.js
@@ -0,0 +1,7 @@
+document.querySelector('header .fa-bars').addEventListener('click', function () {
+    document.querySelector('header nav').classList.toggle('open')
+})
+
+document.querySelector('header .fa-times').addEventListener('click', function () {
+    document.querySelector('header nav').classList.toggle('open')
+})
