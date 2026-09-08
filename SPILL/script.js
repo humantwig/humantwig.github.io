507dd2d3066623c7f1bbd55202902f35fb49a003
Jacob Lucas <57728735+humantwig@users.noreply.github.com>
2026-09-08 13:39:57 +1200

    Delete SPILL directory

--- SPILL/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('header .fa-bars').addEventListener('click', function () {
-    document.querySelector('header nav').classList.toggle('open')
-})
-
-document.querySelector('header .fa-times').addEventListener('click', function () {
-    document.querySelector('header nav').classList.toggle('open')
-})
