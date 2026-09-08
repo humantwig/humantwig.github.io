f22ea05829720190a443e0a4f92b63c32b8f822a
Jacob Lucas <57728735+humantwig@users.noreply.github.com>
2026-09-08 13:41:57 +1200

    Delete new-enterprise directory

--- new-enterprise/WebApp/javascript/script.js
+++
@@ -1,1 +0,0 @@
-M.toast({html: 'I am a toast!'})
--- new-enterprise/js/script.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  document.getElementById("myNav").style.display = "block";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.display = "none";
-}
